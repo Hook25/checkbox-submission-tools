b968fe3b02791051d388af6865f6f018b7dc204a
Pierre Equoy <pierre.equoy@canonical.com>
2025-07-16 18:07:17 +0200

    Convert realtime timestamp to human readable string
    Signed-off-by: Pierre Equoy <pierre.equoy@canonical.com>

--- checkbox_submission_tools/utils.py
+++ checkbox_submission_tools/utils.py
@@ -1,8 +1,19 @@
+import datetime
 from contextlib import suppress
+
+
+def realtime_to_humantime(timestamp_microseconds: int):
+    timestamp_seconds = timestamp_microseconds / 1_000_000
+    dt_object = datetime.datetime.fromtimestamp(timestamp_seconds, tz=datetime.timezone.utc)
+    human_readable_utc = dt_object.strftime("%Y-%m-%d %H:%M:%S UTC")
+    return human_readable_utc
 
 
 def fallback_formatter(formatters: list[str]):
     def _f(x: dict):
+        if x.get("__REALTIME_TIMESTAMP"):
+            timestamp_microseconds = int(x["__REALTIME_TIMESTAMP"])
+            x["__REALTIME_TIMESTAMP"] = realtime_to_humantime(timestamp_microseconds)
         formatters_i = iter(formatters)
         while True:
             try:
